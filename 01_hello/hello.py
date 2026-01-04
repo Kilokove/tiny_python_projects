cd9c15e457215c54726d6956a9798976d02434c7
Kilokove <133579937+Kilokove@users.noreply.github.com>
2026-01-04 09:37:25 -0500

    Continuing work in book

---
+++ 01_hello/hello.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""
+Author: Richard P. Kovach <Richard.Paul.Kovach@gmail.com>
+Purpose: Say hello
+"""
+
+
+import argparse
+
+
+# ----------------------------------------------------------------
+def get_args():
+    """Get command-line arguments"""
+    parser = argparse.ArgumentParser(description='Say hello')
+    parser.add_argument('-n', '--name', metavar='name',
+                        default='World', help='Name to greet')
+
+    return parser.parse_args()
+
+
+# -----------------------------------------------------------------
+def main():
+    """Main function"""
+    args = get_args()
+    print(f"Hello, {args.name}!")
+
+
+# ------------------------------------------------------------------
+if __name__ == '__main__':
+    main()
